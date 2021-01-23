533bb07eb46eff679a3f876a9ce1be332f863c62
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-23 22:19:43 +0530

    aded eyes detection

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import cv2
 
-face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_detector  = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
-eye_detector = cv2.CascadeClassifier('haarcascade_eye.xml')
+eye_detector   = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-webcam = cv2.VideoCapture(0)
+webcam         = cv2.VideoCapture(0)
 
 while True:
     seccessful_frame_read, frame = webcam.read()
     if not seccessful_frame_read:
         break
 
+
     frame_grayscale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
     faces = face_detector.detectMultiScale(frame_grayscale)
-    smile = smile_detector.detectMultiScale(frame_grayscale,1.7,20)
+
+
+
+
 
     for(x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(100,200,50),4)
@@ -22,13 +26,19 @@ while True:
         face_grayscale = cv2.cvtColor(the_face,cv2.COLOR_BGR2GRAY)
 
         smiles = smile_detector.detectMultiScale(face_grayscale,1.7,20)
+        eye = eye_detector.detectMultiScale(frame_grayscale, 1.1, 20)
+        
+
 
         if len(smiles) >0:
             cv2.putText(frame,'smiling',(x,y+h+40),fontScale=3,
                         fontFace=cv2.FONT_HERSHEY_PLAIN,color=(255,255,255)
                         )
-        # for (x_, y_, w_, h_) in smiles:
-        #     cv2.rectangle(the_face, (x_, y_), (x_ + w_, y_ + h_), (50, 50, 200), 4)
+        if len(eye) >0:
+            cv2.putText(frame,'human',(x,y+h+90),fontScale=3,
+                        fontFace=cv2.FONT_HERSHEY_PLAIN,color=(255,255,255)
+                        )
+
 
     # for (x, y, w, h) in smile:
     #     cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 50, 200), 4)
