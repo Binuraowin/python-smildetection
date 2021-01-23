352e46bce8048a8458d0f02e6919325b0c1840b3
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-23 14:02:09 +0530

    detect face

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ webcam = cv2.VideoCapture(0)
 
 while True:
     seccessful_frame_read, frame = webcam.read()
+    if not seccessful_frame_read:
+        break
+
+    frame_grayscale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    faces = face_detector.detectMultiScale(frame_grayscale)
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(100,200,50),4)
 
     cv2.imshow('Smile Detector',frame)
 
