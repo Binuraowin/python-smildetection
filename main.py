f4704d9d02b6381465a1e07799da4fd3d79cce64
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-23 13:37:46 +0530

    aded  webcam

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import cv2
 
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+smile_detector = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+webcam = cv2.VideoCapture(0)
+
+while True:
+    seccessful_frame_read, frame = webcam.read()
+
+    cv2.imshow('Smile Detector',frame)
+
+    cv2.waitKey(1)
